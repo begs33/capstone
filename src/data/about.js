aea88135ddfd4073415c6c51cb5ee96128a7fce6
begs33 <begs33@gmail.com>
2026-02-04 20:13:56 -0600

    End of Feb 4 Class - Next Step is Class 10 Planning

---
+++ src/data/about.js
@@ -0,0 +1,119 @@
+export const aboutData = {
+  // Bio section
+  bio: {
+    intro: "This is where I would write a brief introduction about myself",
+    background: "This is where I would describe my background",
+    current: "This is where I would talk about current projects",
+    personal: "This is where I would share about my personal ideas",
+  },
+
+  values: [
+    {
+      id: 1,
+      title: "User-centered Design",
+      description: "I will format my experiences",
+      iconId: "FaUserCircle",
+    },
+    {
+      id: 2,
+      title: "Clean, Maintainable Code",
+      description: "I will format my experiences",
+      iconId: "FaCode",
+    },
+    {
+      id: 3,
+      title: "Accessibility First",
+      description: "I will format my experiences",
+      iconId: "ImMagicWand",
+    },
+  ],
+
+  experience: [
+    {
+      id: 1,
+      company: "GOAT Corp",
+      role: "Frontend Developer",
+      duration: "Jan 2025 - Present",
+      description: "Developed and maintained the companies website",
+      achievements: ["Item 1", "Item 2", "Goat 3"],
+    },
+    {
+      id: 2,
+      company: "GOAT Corp",
+      role: "Frontend Developer",
+      duration: "Jan 2025 - Present",
+      description: "Developed and maintained the companies website",
+      achievements: ["Item 1", "Item 2", "Goat 3"],
+    },
+    {
+      id: 3,
+      company: "GOAT Corp",
+      role: "Frontend Developer",
+      duration: "Jan 2025 - October 2025",
+      description: "Developed and maintained the companies website",
+      achievements: ["Item 1", "Item 2", "Goat 3"],
+    },
+  ],
+
+  education: [
+    {
+      id: 1,
+      degree: "Google Certification",
+      institution: " Goat School",
+      year: "2019",
+      details: "Work in progress",
+    },
+    {
+      id: 2,
+      degree: "Bootstrap, CSS,",
+      institution: " Goat School",
+      year: "2019",
+      details: "Work in progress",
+    },
+    {
+      id: 3,
+      degree: "JavaScript",
+      institution: " Goat School",
+      year: "2019",
+      details: "Work in progress",
+    },
+  ],
+
+  interests: [
+    {
+      id: 1,
+      name: "DSLR Nikon",
+      description: "Capture life in my perspective",
+      iconId: "FaCamera",
+      image: "http://picsum.photos/300?random=4",
+    },
+    {
+      id: 2,
+      name: "Goats in Motion",
+      description: "Capture life in my perspective",
+      iconId: "FaCamera",
+      image: "http://picsum.photos/300?random=4",
+    },
+    {
+      id: 3,
+      name: "Life of a Goat",
+      description: "Capture life in my perspective",
+      iconId: "FaCamera",
+      image: "http://picsum.photos/300?random=4",
+    },
+  ],
+
+  cta: {
+    heading: "Let's Work Together",
+    description:
+      "Interested in working on a collab or have a project in mind? Feel free to reach out!",
+    primaryAction: {
+      text: "Reach out for contact info",
+      href: "/contact",
+    },
+    secondaryAction: {
+      text: "View my Work",
+      href: "/portfolio",
+    },
+  },
+};
